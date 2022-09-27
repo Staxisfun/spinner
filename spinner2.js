ac5ce22c976af98c8de40905d40669325f1b2d32
Matt Wilson <mwilson82@hotmail.com>
2022-09-27 22:48:34 +0000

    removed old spinner1 code from spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,30 +1,6 @@
 
 
 
-
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
-// }, 700);
-
-// setTimeout(() => {
-//   process.stdout.write('\r|    ')
-// }, 900);
-
-
-
 const spinnerArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ']
 let delay = 100
 
